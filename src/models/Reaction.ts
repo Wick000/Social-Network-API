e34097b641812048aa880fe79f822d5124ac5381
Wick000 <chad.abramson987@gmail.com>
2025-02-26 00:02:12 -0500

    worked on all three models

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -1,2 +1,30 @@
 import { Schema, Document, ObjectId, Types } from 'mongoose';
 //check imports when work starts
+
+interface IReaction extends Document {
+    reactionId: ObjectId;
+    reactionBody: string;
+    username:string;
+    createdAt:Date;
+}
+
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            maxlength:280,
+        },
+        username: {
+            type: String,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+)
+export default reactionSchema;
--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -1,2 +1,41 @@
-import { Schema, Document, ObjectId, Types } from 'mongoose';
+import { Schema, Document, ObjectId, Types, model } from 'mongoose';
 //check imports when work start
+import Reaction from './Reaction.js'
+interface IThoughts extends Document {
+    thoughtText: string;
+    createdAt: Date;
+    username: string;
+    reactions: Reaction[];
+}
+
+const thoughtsSchema = new Schema<IThoughts>(
+    {
+      thoughtText: {
+        type: String,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      username: {
+        type: String,
+      },
+      reactions: [Reaction],  
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
+
+
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+export default Thoughts;
