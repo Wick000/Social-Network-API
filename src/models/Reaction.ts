31dc54f3dc053a793f2b5f01f8ed26738f4ce3f8
Wick000 <chad.abramson987@gmail.com>
2025-02-25 22:37:52 -0500

    worked on user.ts

---
+++ src/models/Reaction.ts
@@ -0,0 +1,2 @@
+import { Schema, Document, ObjectId, Types } from 'mongoose';
+//check imports when work starts
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,37 @@
 import { Schema, Document, model, ObjectId } from 'mongoose';
+//check imports when work starts
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        username: String,
+        email: String,
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('User', userSchema)
+
+export default User;
