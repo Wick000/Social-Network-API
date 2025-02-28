242d33fae9b7928a6f2dbd4c1f4b0fdb3f0a2ae8
Wick000 <chad.abramson987@gmail.com>
2025-02-28 02:40:22 -0500

    added thought Routes

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+const router = Router();
+
+import {
+    getAllThoughts,
+    getSingleThoughts,
+    createNewThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction,
+} from '../../controllers/thoughtController.js';
+
+router.route('/api/thoughts')
+.get(getAllThoughts).get(getSingleThoughts).post(createNewThought).put(updateThought).delete(deleteThought);
+
+router.route('/api/thoughts/:thoughtId/reactions').post(createReaction).delete(removeReaction);
